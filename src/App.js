fb8bd46412d57f114823626d269f76a8c97dc673
Minyoung Lee <leemin02_@naver.com>
2021-11-17 01:03:57 +0900

    1) SetUset 페이지, TextExample페이지 개발,  2) Text페이지 디버깅

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React, {useState, useEffect} from 'react'
 import { Link, Route, useParams } from 'react-router-dom';
 // import './css/reset.css';
+import './css/app.css';
 import axios from 'axios';
+
 const key = '43c8e52955dbc4c8d2b69e98c6d641f2';
 
 function SetUser(){
   const [username, setUsername] = useState()
   const [gender, setGender] = useState()
-  
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  }
+  const getGender = (e) => {
+    setGender(e.target.value);
+    console.log(gender)
+  }
+
+  //버튼은 className={isActive ? "activebtn" : "unactivebtn"} 으로 활성화
+
   return(
     <>
     <h1>직업가치관검사</h1>
-    <form>
-      이름: <input type="text" id="username" value={username}/>
+    <form type="submit">
+      이름: <input
+        type="text"
+        id="username"
+        value={username}
+        onChange={handleUsername}
+        />
       <br/>
       성별<br/>
-      <input type="radio" id="male" name="gender" value={gender}></input>
+      <input type="radio" id="male" name="gender" value="male" onClick={getGender}/>
       <label for="male">남자</label>
       <br/>
-      <input type="radio" id="female" name="gender" value={gender}></input>
+      <input type="radio" id="female" name="gender" value="female" onClick={getGender}></input>
       <label for="female">여자</label>
       <br/>
-      <button type="button">검사 시작</button>
+      <button type="button"><Link to='/testExample'>검사 시작</Link></button>
     </form>
     </>
   )
@@ -32,6 +48,19 @@ function SetUser(){
 function TestExample(){
   return(
     <>
+    <h2>검사예시</h2>
+    <h3>0%</h3>
+    <p>
+      직업과 관련된 두개의 가치 중에서 자기에게 더 중요한 가치에 표시하세요.<br/>
+      가치의 뜻을 잘모르겠다면 문항 아래에 있는 가치의 설명을 확인해보세요.
+    </p>
+    <h3>두개 가치 중에 자신에게 더 중요한 가치를 선택하세요.</h3>
+    <input type="radio" name="answer" value="answer"></input>
+    <label for="answer">능력발휘</label>
+    <input type="radio" name="answer" value="answer"></input>
+    <label for="answer">자율성</label>
+    <br/>
+    <button type="button"><Link to='/test'>검사시작</Link></button>
     </>
   )
 }
@@ -39,7 +68,7 @@ function TestExample(){
 
 
 function Test(){
-  const [saveData, setSaveData] = useState()
+  const [saveData, setSaveData] = useState([])
   const [countPer, setCountPer] = useState(0)
 
   useEffect(() => {
@@ -55,18 +84,27 @@ function Test(){
     loadQuestion();
   }, []);
 
-  
+  const questions = saveData.map((item) => {
+    return(<>
+      <h3>{item.qitemNo}. {item.question}</h3>
+      <input type="radio" name="answer" value="answer"></input>
+      <label for="answer">{item.answer01}</label>
+      <input type="radio" name="answer" value="answer"></input>
+      <label for="answer">{item.answer02}</label>
+      <input type="radio" name="answer" value="answer"></input>
+      <label for="answer">{item.answer03}</label>
+      <input type="radio" name="answer" value="answer"></input>
+      <label for="answer">{item.answer04}</label>
+      <input type="radio" name="answer" value="answer"></input>
+      <label for="answer">{item.answer05}</label>
+      </>)
+  });
   
   return (
   <>
     <h2>검사 진행</h2>
     <h3>{countPer}%</h3>
-    { saveData && saveData.length > 0 ?
-    (<>
-    <h3>{saveData[0].qitemNo}. {saveData[0].question}</h3>
-    <input type="radio" name="answer" value="answer"></input>
-    <label for="answer">{saveData[0].answer01}</label>
-    </>) : undefined }
+    { saveData && saveData.length > 0 ? questions : undefined }
     <br/>
   </>
 )}
