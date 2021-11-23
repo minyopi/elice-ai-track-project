eeb084c2f1bd2d2a16b61769cc5b2fe48862b6d9
Minyoung Lee <leemin02_@naver.com>
2021-11-23 17:44:23 +0900

    2021.11.23 중간 백업 - Result 페이지 작업중 GET요청시 에러 디버깅 완료

--- src/components/Result.js
+++ src/components/Result.js
@@ -13,8 +13,9 @@ export function Result() {
     async function loadResult(){
       try {
         const response = await axios.post(`http://www.career.go.kr/inspct/openapi/test/report?apikey=${context.apikey}&qestrnSeq=6`, context);
-        setData(response.data.RESULT);
+        setData(response.data['RESULT']);
         setSeq(data.url.split("seq=")[1]);
+        console.log(seq)
       } catch (e) {
         console.log('POST 요청에서 에러 발생');
       }
@@ -29,7 +30,7 @@ export function Result() {
           headers: { 'Access-Control-Allow-Origin': '*' }
         };
         const response2 = await axios.get(`https://www.career.go.kr/inspct/api/psycho/report?seq=${seq}`, config);
-            setJsonData(response2['data'])
+        setJsonData(response2['data'])
       } catch (error) {
         console.log("JSON Data GET요청에서 에러 발생")
       }
@@ -41,6 +42,8 @@ export function Result() {
     <>
       <h1>직업가치관검사 결과표</h1>
       <p>직업가치관이란 직업을 선택할 때 영향을 끼치는 자신만의 믿음과 신념입니다. 따라서 여러분의 직업생활과 관련하여 포기하지 않는 무게중심의 역할을 한다고 볼 수 있습니다. 직업가치관검사는 여러분이 직업을 선택할 때 상대적으로 어떠한 가치를 중요하게 생각하는지를 알려줍니다. 또한 본인이 가장 중요하게 생각하는 가치를 충족시켜줄 수 있는 직업에 대해 생각해 볼 기회를 제공합니다.</p>
+      { jsonData ?
+      <>
       <table border="1">
         <th>이름</th>
         <th>성별</th>
@@ -48,13 +51,14 @@ export function Result() {
         <tr>
           <td>{context.name}</td>
           <td>{context.gender === "100323" ? "남" : "여"}</td>
-          <td>{jsonData.result['endDtm'].slice(0,10).split("-").join(".")}</td>
+          <td>{jsonData.result.endDtm.slice(0,10).split("-").join(".")}</td>
         </tr>
       </table>
+      </>
+      : undefined}
       <h2>직업가치관 결과</h2>
       <h2>가치관과 관련이 높은 직업</h2>
-      <Link to='/finishTest'><button>이전</button></Link>
-      <Link to='/'><button>처음으로</button></Link>
+      <Link to='/'><button>다시 검사하기</button></Link>
     </>
   );
 }
