60b3c4effe824a183eb643b8500581b94178dd39
MINYOUNG LEE <leemin02_@naver.com>
2022-03-22 20:40:14 +0900

    build 파일 수정

--- src/store/user.js
+++ src/store/user.js
@@ -1,26 +1,27 @@
 import React, { createContext } from "react";
-import{ apikey }from './key'
+import { apikey } from "./key";
 
 // user 정보를 담을 context를 만드는 곳
 
 export const UserContext = createContext();
 
 const UserStore = (props) => {
-    const users = {
-        "apikey" : apikey,
-        "qestrnSeq" : "6",
-        "trgetSe" : "100209",
-        "name": "",
-        "gender": "",
-        "school": "",
-        "grade": "",
-        "email": "",
-        "startDtm": 1550466291034,
-        "answers": ""
-}
+  const users = {
+    apikey: apikey,
+    qestrnSeq: "6",
+    trgetSe: "100209",
+    name: "",
+    gender: "",
+    school: "",
+    grade: "",
+    email: "",
+    startDtm: 1550466291034,
+    answers: "",
+  };
 
-    return (<UserContext.Provider value={users}>{props.children}</UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider value={users}>{props.children}</UserContext.Provider>
+  );
 };
 
 export default UserStore;
